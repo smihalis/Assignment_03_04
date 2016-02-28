663b8cb8ecd6a0c2c9992aae77c7064a4dc2002c
smihalis <s_mihalis@hotmail.com>
2016-02-28 11:39:22 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 run_analysis<- function(){
-  #Get data from test team and combine them in a dataframe
+  #Get data from test team, calculate mean and standard deviation and combine them in a dataframe
   sub.t1<- read.table("test\\subject_test.txt")
   set1<- read.table("test\\x_test.txt")
   set1.av<-apply(set1,1,mean)
@@ -7,7 +7,7 @@ run_analysis<- function(){
   act1<- read.table("test\\y_test.txt")
   df1<- data.frame(activityCode=act1[[1]],subject=sub.t1[[1]],mean=set1.av,s.d=set1.sd)
   
-  #Get data from train team and combine them in a dataframe
+  #Get data from train team, calculate mean and standard deviation and combine them in a dataframe
   sub.t2<- read.table("train\\subject_train.txt")
   set2<- read.table("train\\x_train.txt")
   set2.av<-apply(set2,1,mean)
@@ -15,10 +15,10 @@ run_analysis<- function(){
   act2<- read.table("train\\y_train.txt")
   df2<- data.frame(activityCode=act2[[1]],subject=sub.t2[[1]],mean=set2.av,s.d=set2.sd)
   
-  #Combine the two dataframes together
+  #Join the two dataframes together
   df<- rbind(df1,df2)
   
-  #Get the activity labels and the corresponding code
+  #Get the activity labels and the corresponding code, rename columns for readability
   act.lbl<- read.table("activity_labels.txt")
   act.lbl<- rename(act.lbl,activityCode=V1,activity=V2)
 
